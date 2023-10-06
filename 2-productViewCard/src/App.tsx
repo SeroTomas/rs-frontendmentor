8d3b1d8c876434531cfaed0f57021692ed75bc5c
SeroTomas <serocontacto@gmail.com>
2023-10-06 16:37:45 -0300

    structure of info still working

--- 2-productViewCard/src/App.tsx
+++ 2-productViewCard/src/App.tsx
@@ -1,13 +1,15 @@
 import style from "./App.module.css";
 import CustomImage from "./components/CustomImage/CustomImage";
+import Info from "./components/Info/Info";
 
 function App() {
 
   return (
-    <div className={style.card}>
-      <CustomImage/>
-      
-
+    <div className={style.screen}>
+      <div className={style.card}>
+        <CustomImage />
+        <Info/>
+      </div>
     </div>
   )
 }
--- 2-productViewCard/src/components/Info/Info.tsx
+++ 2-productViewCard/src/components/Info/Info.tsx
@@ -1,13 +1,24 @@
-import { useState, useEffect } from "react";
 import style from "./info.module.css";
 
-
-
 const Info = () => {
-    
+
 
     return (
         <div>
+            <p>PERFUME</p>
+            <div>
+                <h2>
+                    Gabrielle Essence Eau De Parfum
+                </h2>
+                <p>
+                    A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
+                </p>
+
+                <div className={style.prices}>
+                    <span>$149.99</span>
+                    <span className={}>$169.99</span>
+                </div>
+            </div>
         </div>
     )
 }
