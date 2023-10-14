06aa2f000ed74040052ad2395dc66503f7f6f870
Tomas Silverio <serocontacto@gmail.com>
2023-10-14 17:56:05 -0300

    add classnames

--- 2-productViewCard/src/components/Info/Info.tsx
+++ 2-productViewCard/src/components/Info/Info.tsx
@@ -4,21 +4,22 @@ const Info = () => {
 
 
     return (
-        <div>
-            <p>PERFUME</p>
+        <div className={style.infoContainer}>
+            <p className={style.sectionTitle}>PERFUME</p>
             <div>
-                <h2>
+                <h2 className={style.productName}>
                     Gabrielle Essence Eau De Parfum
                 </h2>
-                <p>
+                <p className={style.productDescription}>
                     A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
                 </p>
 
                 <div className={style.prices}>
-                    <span>$149.99</span>
-                    <span className={}>$169.99</span>
+                    <span className={style.newPrice}>$149.99</span>
+                    <span className={style.oldPrice}>$169.99</span>
                 </div>
             </div>
+            <button className={style.cartButton}>add to Cart</button>
         </div>
     )
 }
